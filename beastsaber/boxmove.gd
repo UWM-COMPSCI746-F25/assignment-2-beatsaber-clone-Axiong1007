37808c5bb32b927061f658f686e2ad6b15603fe6
Anna Xiong <xion2254@uwm.edu>
2025-10-15 18:21:42 -0500

    box stuff

---
+++ beastsaber/boxmove.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+@export var dir = Vector3(0,0,-1)
+@export var speed = 0.5
+var location = -10
+
+func _process(delta):
+	global_position += dir * (speed * delta)
--- beastsaber/scripts/raycast.gd
+++ beastsaber/scripts/raycast.gd
@@ -1,7 +1,13 @@
 extends XRController3D
 
-@export var ray_length = 10.0
+@export var ray_length = 1.0
 @export var enabled = true
+
+var origin = global_position
+var dir = global_basis.z*-1
+var end = origin + (dir * ray_length)
+var query = PhysicsRayQueryParameters3D.create(origin, end)
+var collision_enabled = true
 
 # Reference to the line renderer child node
 var line_renderer
@@ -11,15 +17,21 @@ var line_renderer
 func _physics_process(delta):
 	var space_state = get_world_3d().direct_space_state
 	
-	var origin = global_position
-	var dir = global_basis.z*-1
-	var end = origin + (dir * ray_length)
-	var query = PhysicsRayQueryParameters3D.create(origin, end)
-	query.collide_with_areas = true
-	var result = space_state.intersect_ray(query)
+	origin = global_position
+	dir = global_basis.z*-1
+	end = origin + (dir * ray_length)
+	query = PhysicsRayQueryParameters3D.create(origin, end)
+	query.collide_with_areas = collision_enabled
 	
-	if result:
-		print("Collision with ", result.collider.name)
+	
+	if collision_enabled:
+		var result = space_state.intersect_ray(query)
+		if result:
+			print("Collision with ", result.collider.name)
+			var name = result.collider.name
+			if name == "blue":
+				$hit.play()
+				result.collider.queue_free()
 		
 		
 	$"rightbeam".points[0] = origin
@@ -27,5 +39,8 @@ func _physics_process(delta):
 func _on_button_pressed(name):	
 	if (name == "ax_button"): 
 		$"rightbeam".visible = !$"rightbeam".visible
-		
+		collision_enabled = !collision_enabled
+		if collision_enabled:
+			$onsaberright.play()
+		query.collide_with_areas = collision_enabled
 		print("right button press")
